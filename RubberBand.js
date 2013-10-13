1fbd0761b7caed34f02e011a3ce3d3388dbd10c1
DevDavido <bigboy29@freenet.de>
2013-10-14 00:06:17 +0200

    Added $body variable (minimal performance improvement) and comment docs, renamed variables to camelCase

--- RubberBand.js
+++ RubberBand.js
@@ -1,5 +1,11 @@
-(function(window,undefined){
-  
+/**
+* RubberBand
+* Version: 1.0
+* http://thrivingkings.com/read/RubberBand-Easily-add-the-iOS-style-pull-to-refresh-functionality-to-any-page
+*/
+
+;(function(window, undefined) {
+
 	// RubberBand, the object
 	var RubberBand = function(cb) {
 		
@@ -7,54 +13,52 @@
 		var $me = this;
 		
 		this.config = {
-			pull_text: 'Pull to Refresh',
-			load_text: 'Loading...'
+			pullText: 'Pull to Refresh',
+			loadText: 'Loading...'
 		};
 		
 		// variables to be used throughout that are set on init (don't edit)
 		this.variables = {
 			callback: null,
 			calling: false,
-			e_height: null,
-			max_height: null,
-			original_top: null,
+			elementHeight: null,
+			maxHeight: null,
+			originalTop: null,
 			scrolling: null,
 			closing: null,
 			inplay: false,
 			$RBe: null,
 			images: true,
+                        $body: null
 		};
 		
 		// Initialize function
 		// sets all variables
 		this.init = function(cb) {
 			
+			$me.variables.$body = $('body');
+                        
 			// if the element doesn't exist, create it
-			if(!$('#RubberBandjs').length) {
+			if (!$('#rubberband').size()) {
 				
-				$('body').prepend( $('<div id="RubberBandjs">')
-					.append( $('<div class="rband">')
-						.append( $('<h3 class="text">') ) 
-					)
-				);
+				$me.variables.$body.prepend($('<div id="rubberband">')
+                                                   .append($('<div class="band">')
+                                                   .append($('<h3 class="text">'))));
 				$me.variables.images = false;
 			}
 			
 			// set all necessary variables
-			$me.variables.$RBe = $('#RubberBandjs');
-			
-			$me.variables.$RBe.find('.rband .text').html($me.config.pull_text);
-			
+			$me.variables.$RBe = $('#rubberband');
+			$me.variables.$RBe.find('.band .text').html($me.config.pullText);
+                        
 			$me.variables.callback = cb;
-			
-			$me.variables.e_height = parseFloat( $me.variables.$RBe.css('top').replace(/px/, '') ); // this needs to be an integer
-			
-			$me.variables.max_height = Math.abs( $me.variables.e_height ); // swap to positive
-			
-			$me.variables.original_top = $me.variables.e_height;
+                        
+			$me.variables.elementHeight = parseFloat($me.variables.$RBe.css('top').replace(/px/, '')); // this needs to be an integer
+			$me.variables.maxHeight = Math.abs($me.variables.elementHeight); // swap to positive
+			$me.variables.originalTop = $me.variables.elementHeight;
 			
 			// Bind to the scrolling of the window
-			$(window).scrollTop(1).on("scroll touchmove", $me.RB);
+			$(window).scrollTop(1).on('scroll touchmove', $me.RB);
 		};
 		
 		// The actual RubberBand function
@@ -64,74 +68,60 @@
 			$me.variables.scrolling = window.clearTimeout($me.variables.scrolling);
 			
 			// only do something if they're at the top of the page
-			if($(window).scrollTop()<=0) {
+			if ($(window).scrollTop() <= 0) {
 			
 				$me.variables.inplay = true;
 				
 				var diff;
 			
-				if($me.variables.e_height<0) {
+				if ($me.variables.elementHeight < 0) {
 					
 					// tension control for more of a natural feel
-					if($me.variables.e_height>=-10) {
-				
+					if ($me.variables.elementHeight >= -10) {
 						diff = 3;
-					} else if($me.variables.e_height>=-20) {
-				
+					} else if ($me.variables.elementHeight >= -20) {
 						diff = 4;
-					} else if($me.variables.e_height>=-50) {
-				
+					} else if ($me.variables.elementHeight >= -50) {
 						diff = 5;
 					} else {
-				
 						diff = 6;
 					}
 					
 					// determine values and set css rules
-					$me.variables.e_height = $me.variables.e_height + diff;
-				
-					$me.variables.$RBe.css('top', $me.variables.e_height +'px');
-				
-					$('body').css('padding-top', ($me.variables.max_height + $me.variables.e_height) +'px');
+					$me.variables.elementHeight = $me.variables.elementHeight + diff;
+					$me.variables.$RBe.css('top', $me.variables.elementHeight + 'px');
+					$me.variables.$body.css('padding-top', ($me.variables.maxHeight + $me.variables.elementHeight) + 'px');
 					
 					// this allows the user to continue to scroll
 					$(window).scrollTop(1);
 				
 				// when the very top has been reached, "refresh"
-				} else if($me.variables.e_height>=0) {
+				} else if ($me.variables.elementHeight >= 0) {
 					
 					// no more scrolling for now
-					$(window).unbind("scroll");
+					$(window).unbind('scroll');
 					
 					// set CSS to avoid "over-scrolling"
-					$me.variables.$RBe.css('top', '0px');
+					$me.variables.$RBe.css('top', '0');
 				
-					$('body').css('padding-top', $me.variables.max_height +'px');
-				
+					$me.variables.$body.css('padding-top', $me.variables.maxHeight + 'px');
 					$me.variables.scrolling = window.clearTimeout($me.variables.scrolling);
-					
-					$me.variables.$RBe.find('.rband .text').html($me.config.load_text);
+					$me.variables.$RBe.find('.band .text').html($me.config.loadText);
 					
 					// switch images, if they exist
-					if($me.variables.images) {
-					
-						$me.variables.$RBe.find('.rband .load').show();
-						$me.variables.$RBe.find('.rband .arrow').hide();
+					if ($me.variables.images) {
+						$me.variables.$RBe.find('.band .load').show();
+						$me.variables.$RBe.find('.band .arrow').hide();
 					}
 					
 					// fire the callback, or "on refresh" function
-					if($me.variables.callback) {
-						
-						if(!$me.variables.calling) {
-							
+					if ($me.variables.callback) {
+						if (!$me.variables.calling) {
 							$me.variables.callback($me);
-							
 							$me.variables.calling = true;
 						}
-						
 					// otherwise just close it
 					} else {
-						
 						$me.close();
 					}
 					
@@ -139,12 +129,9 @@
 			}
 		
 			// if they're not scrolling, the band is showing, and it isn't already closing-- close it
-			if(!$me.variables.closing && $me.variables.inplay && $me.variables.e_height<0) {
-				
+			if (!$me.variables.closing && $me.variables.inplay && $me.variables.elementHeight < 0) {
 				$me.variables.scrolling = window.setTimeout(function() {
-					
 					$me.close();
-				
 				}, 200);
 			}
 		};
@@ -154,34 +141,38 @@
 		this.close = function() {
 			
 			// fire both animations
-			$me.variables.$RBe.animate({ top: $me.variables.original_top }, 200);
+			$me.variables.$RBe.animate({ top: $me.variables.originalTop }, 200);
 			
 			// reset after the animation has completed
-			$('body').animate({ paddingTop: 0 }, 200, function() { 
+			$me.variables.$body.animate({ paddingTop: 0 }, 200, function() { 
 			
 				//$(window).scrollTop(1); 
 			
-				$(window).on("scroll touchmove", $me.RB); 
+				$(window).on('scroll touchmove', $me.RB); 
 			
-				$me.variables.e_height = $me.variables.original_top;
-		
+				$me.variables.elementHeight = $me.variables.originalTop;
 				$me.variables.inplay = false;
-				
 				$me.variables.calling = false;
-			
 				$me.variables.closing = window.clearTimeout($me.variables.closing);
 				
 				// reset action text
-				$me.variables.$RBe.find('.rband .text').html($me.config.pull_text);
+				$me.variables.$RBe.find('.band .text').html($me.config.pullText);
 				
-				if($me.variables.images) {
-				
-					$me.variables.$RBe.find('.rband .load').hide();
-					$me.variables.$RBe.find('.rband .arrow').show();
+				if ($me.variables.images) {
+					$me.variables.$RBe.find('.band .load').hide();
+					$me.variables.$RBe.find('.band .arrow').show();
 				}
 			});
 
 		};
+                
+                // Remove function
+                this.remove = function() {
+                    $(window).off('scroll touchmove');
+                    $me.variables.$body.finish();
+                    
+                    delete $me;
+                };
 		
 		// Runtime
 		this.init(cb);
